900b6bc4aaf049956fb1428e5fd3a4d8fd48d560
abdiansyah <xsis.abdiansyahrizki@xl.co.id>
2022-09-23 19:54:40 +0700

    options headless

--- demoMidtrans.py
+++ demoMidtrans.py
@@ -8,6 +8,8 @@ import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option('excludeSwitches', ['--headless'])
+options.add_experimental_option('excludeSwitches', ['--no-sandbox'])
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get("https://demo.midtrans.com/")
 driver.maximize_window
